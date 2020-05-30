8b8b53182d2272ec6b0894ae498935cef26e6199
Youngmin Park <codepark2019@gmail.com>
2020-05-30 01:45:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
 def my_collect(array)
 
+  modified_array = []
   index = 0
   while index < array.length
-    yield(array[index])
+    modified_array << yield(array[index])
     index += 1
+  end
+  modified_array
 end
 
 # array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
