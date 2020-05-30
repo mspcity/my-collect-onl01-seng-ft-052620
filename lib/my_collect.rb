9f03b8945b4a386037281e2e280034049b1b994c
Youngmin Park <codepark2019@gmail.com>
2020-05-30 01:35:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 def my_collect(array)
+
+  index = 0
+  while index < array.length
+    yield(array[index])
+    index += 1
 end
+
+# array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+# my_collect(array) do |name|
+#   name.split(" ").first
+# end
+
+# returns ["Tim", "Tom", "Jim"]
